ceaaf573baeb29ac77fb9325fd0eba24a848fab8
Ryan Davis <epikit@gmail.com>
2015-06-09 16:24:07 -0500

    Create hxpress.http.IncomingMessage type, an extern to http.IncomingMessage

---
+++ src/hxpress/http/IncomingMessage.hx
@@ -0,0 +1,27 @@
+package hxpress.http;
+
+import haxe.Constraints.Function;
+import haxe.extern.EitherType;
+import haxe.extern.Rest;
+
+import tannus.ds.Object;
+import tannus.sys.Path;
+import hxpress.http.EventEmitter;
+
+@:jsRequire('http', 'IncomingMessage')
+extern class IncomingMessage extends EventEmitter {
+/* === Instance Fields === */
+	var httpVersion : String;
+	
+	var headers : Object;
+	var rawHeaders : Array<String>;
+	var trailers : Object;
+	var rawTrailers : Array<String>;
+
+	var method : Null<String>;
+	var url : Null<Path>;
+
+/* === Instance Methods === */
+
+	function setTimeout(ms:Int, cb:Function):Void;
+}
