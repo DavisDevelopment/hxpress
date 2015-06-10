7b6abf19eed186e5a8c73765462835256ecc58bb
Ryan Davis <epikit@gmail.com>
2015-06-10 00:33:16 -0500

    Added "request" method to hxpress.nhttp.Http class

--- src/hxpress/nhttp/Http.hx
+++ src/hxpress/nhttp/Http.hx
@@ -1,10 +1,16 @@
 package hxpress.nhttp;
 
-import hxpress.nhttp.*;
+import hxpress.http.*;
 import haxe.Constraints.Function;
+import haxe.extern.EitherType;
+
+import tannus.ds.Object;
 
 @:jsRequire('http')
 extern class Http {
 	/* Create a new Server */
 	static function createServer(?onreq : Function):Server;
+
+	/* Create a new Request */
+	static function request(opts:EitherType<String, Object>, ?callback:IncomingMessage->Void):ClientRequest;
 }
