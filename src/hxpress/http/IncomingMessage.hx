ce1b5da71ca91a36bf63eb2232307961cd25af79
Ryan Davis <epikit@gmail.com>
2015-06-10 00:41:49 -0500

    Created hxpress.http.IncomingMessage, which is an abstract built on top of hxpress.nhttp.IncomingMessage to add utility methods for common and/or overly verbose actions

---
+++ src/hxpress/http/IncomingMessage.hx
@@ -0,0 +1,68 @@
+package hxpress.http;
+
+import hxpress.nhttp.IncomingMessage in Msg;
+import hxpress.util.Buffer;
+
+import tannus.ds.Object;
+import tannus.ds.Maybe;
+import tannus.io.ByteArray;
+import tannus.node.Url;
+
+import Std.is;
+
+@:forward
+abstract IncomingMessage (Msg) from Msg to Msg {
+	/* Constructor Function */
+	public inline function new(m : Msg) {
+		this = m;
+	}
+
+/* === Instance Fields === */
+
+	/**
+	  * The MIME Type of [this] response
+	  */
+	public var type(get, never):Null<String>;
+	private inline function get_type():String {
+		return ('' + this.headers['content-type']);
+	}
+
+	/**
+	  * The URL being requested
+	  */
+	public var url(get, never):UrlData;
+	private inline function get_url():UrlData {
+		return Url.parse(this.url);
+	}
+
+	/**
+	  * The GET parameters of [this] request
+	  */
+	public var params(get, never):Object;
+	private inline function get_params():Object {
+		return url.query;
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Get the data being sent on [this] Message
+	  */
+	public function data(cb : ByteArray->Void):Void {
+		/* ByteArray to hold any/all data received */
+		var buff:ByteArray = new ByteArray();
+
+		/* when a chunk of  data is received */
+		this.on('data', function(chunk : Buffer) {
+
+			//- append that chunk to [buff]
+			buff.write( chunk );
+		});
+
+		/* when the end of the data has been reached */
+		this.on('end', function() {
+			//- invoke [cb] with [buff] as it's argument
+			cb( buff );
+		});
+	}
+}
